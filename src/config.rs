c8c551fbf5972bc4d22a50dd472fc1f4a04eac13
qiruizheng <4271439+qiruizheng@users.noreply.github.com>
2025-08-03 17:45:58 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["1.92.124.175"];
-pub const RS_PUB_KEY: &str = "pSBqdL+nEcgnTpbzOYlcMN15gJquA0QuNHwzu5yWaKI=";
+pub const RS_PUB_KEY: &str = "PEuK3nfgbUPnBo90bKZ8Boj3eeIewirT7DPkF+hxdE8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
